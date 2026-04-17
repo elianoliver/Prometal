9f2806892064e15ace46f604092534ebca354d0c
Elian Oliveira <elianoliveira234@gmail.com>
2026-04-17 00:40:07 -0300

    Adicionar arquivo de configuração de ambiente

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  emailjs: {
+    serviceId: 'service_t6i0p69',
+    templateId: 'template_dnj5q5x',
+    publicKey: '3KqwN1_h85S_T0U8S',
+  },
+};
